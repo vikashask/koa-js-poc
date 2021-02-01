75163dfb0001deb1eabbdc5752557a898fe1c7aa
vikash <vikashraj144@gmail.com>
2021-02-01 10:01:09 +0530

    app list

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
-var koa = require("koa");
-var app = new koa();
+// const render = require("./lib/render");
+const logger = require("koa-logger");
+const router = require("@koa/router")();
+const koaBody = require("koa-body");
 
-app.use(function* () {
-  this.body = "Hello world!";
-});
+const Koa = require("koa");
+const app = (module.exports = new Koa());
 
-app.listen(3000, function () {
-  console.log("Server running on https://localhost:3000");
-});
+const posts = [
+  {
+    id: 1,
+    name: "vikask",
+    Country: "India",
+  },
+  {
+    id: 2,
+    name: "test",
+    Country: "Pak",
+  },
+];
+
+// middleware
+app.use(logger());
+
+app.use(koaBody());
+// route definitions
+router
+  .get("/", list)
+
+app.use(router.routes());
+
+function list(ctx) {
+  ctx.response.body = { posts: posts };
+}
+// listen
+
+if (!module.parent) app.listen(3000);
